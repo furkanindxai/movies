32a22d18048fbe869d21a669da4cabef283c216e
Furkan <furkan.nul.aalam@indxtechnology.com>
2023-12-26 11:24:05 +0530

    Set token expiry to 30 mins in generateAccessToken.

--- src/helpers/generateAccessToken.js
+++ src/helpers/generateAccessToken.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 import jwt from "jsonwebtoken";
 
 function generateAccessToken(email) {
-  return jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET);
+  return jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '30m'});
 }
 
 export default generateAccessToken;
