e5739b7f4acd82f0308b9b5d57a715e9c1162af1
Furkan <furkan.nul.aalam@indxtechnology.com>
2024-01-15 23:53:12 +0530

    Add sequelize, set directories for it.

---
+++ src/config/dbConfig.js
@@ -0,0 +1,9 @@
+export default {
+    development: {
+      username: 'postgres',
+      password: 'password',
+      database: 'movies',
+      host: 'localhost',
+      dialect: 'postgres',
+    },
+  };
---
+++ src/models/user.js
@@ -0,0 +1,12 @@
+import { DataTypes } from 'sequelize';
+
+import sequelize from '../db/index.js';
+
+class User extends Model {
+   
+}
+
+User.init({});
+
+
+export default User;
